836960820fca9a2910b465b16b92d0c8dae9855d
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-22 10:11:15 +0300

    sc:search - add a function to handle search queries

--- index.js
+++ index.js
@@ -29,3 +29,31 @@ async function CuratedPhotos()
 
   
 }
+
+
+
+async function searchingPhotos(value)
+{
+    let inputValue = searchInput.value;
+
+    const data = await fetch(`https://api.pexels.com/v1/search?query=${inputValue}&per_page=9`, 
+    {
+        method:"GET",
+        headers:{
+            Authorization:authorizationKey
+        }
+    }
+    );
+    
+    const foundPics = await data.json();
+    console.log(foundPics);
+    foundPics.photos.forEach(foundPic =>{
+        let column2 = document.createElement('div')
+        column2.className = 'mb-3 col-md-3 col-lg-4 col-sm-12'
+        column2.innerHTML = `<img src = ${foundPic.src.large}>`
+        displaySection.appendChild(column2) 
+
+    })
+
+  
+}
