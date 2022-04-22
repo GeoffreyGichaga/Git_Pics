bf2f2f80121b55b3551773303624f36db5bcb9dc
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-22 10:06:59 +0300

    scope:element reference - import all the buttons and inputs to index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const authorizationKey ="563492ad6f91700001000001a8f71b5cd5a54494a9ccf2bb5df9ed5b"
+
+const searchInput = document.getElementById("searchinput")
+const searchButton = document.getElementById("searchbtn")
+const displaySection = document.getElementById("displayProjects")
+const displayImage = document.querySelectorAll(".projectsDisplay")
+
+
