1df45cd01ecd0a3dcdd2adfc2b15cc9be2a05da3
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-22 10:09:49 +0300

    sc:fetch pics - add a function to fetch curated photots from pexel and append on the project display section

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ const displaySection = document.getElementById("displayProjects")
 const displayImage = document.querySelectorAll(".projectsDisplay")
 
 
+async function CuratedPhotos()
+{
+    const data = await fetch(`https://api.pexels.com/v1/curated?per_page=9`, 
+    {
+        method:"GET",
+        headers:{
+            Authorization:authorizationKey
+        }
+    }
+    );
+    
+    const foundPics = await data.json();
+    console.log(foundPics);
+    foundPics.photos.forEach(foundPic =>{
+        let column = document.createElement('div')
+        column.className = 'mb-3 col-md-3 col-lg-4 col-sm-12'
+        column.innerHTML = `<img src = ${foundPic.src.large}>`
+        displaySection.appendChild(column) 
+
+    })
+
+  
+}
